5593dde17067ac272995ea7347ac2a0a90a8fb75
priks <gautamprikshit1@gmail.com>
2022-11-30 07:41:40 +0530

    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-use std::{fs, env};
 use std::error::Error;
+use std::{env, fs};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents: String = fs::read_to_string(config.filename)?;
 
-    let results:Vec<&str> = if config.case_sensitive {
+    let results: Vec<&str> = if config.case_sensitive {
         search(&config.query, &contents)
     } else {
         search_case_insensitive(&config.query, &contents)
@@ -26,10 +26,8 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-pub fn search_case_insensitive<'a>(query: &str,
-    contents: &'a str,
-) -> Vec<&'a str> {
-    let query:String = query.to_lowercase();
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query: String = query.to_lowercase();
     let mut results = Vec::new();
 
     for line in contents.lines() {
@@ -54,7 +52,11 @@ impl Config {
         let filename: String = args[2].clone();
         let case_sensitive: bool = env::var("CASE_INSENSITIVE").is_err();
 
-        Ok(Config { query, filename, case_sensitive })
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -63,15 +65,24 @@ mod test {
     use super::*;
     #[test]
     fn case_sensitive() {
-        let query: &str = "duct";
+        let query: &str = "tape";
         let contents: &str = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.
-        Duct Tape.";
+Rust:
+safe, fast, productive.
+Pick three.
+Duct Tape.";
 
-        assert_eq!(vec!["Rust:", "Trust me."],
-            search_case_insensitive(query, contents));
+        assert_eq!(vec!["Duct Tape."], search_case_insensitive(query, contents));
+    }
+
+    #[test]
+    fn test_search() {
+        let query: &str = "three";
+        let contents: &str = "\
+Rust:
+safe, fast, productive.
+Pick three
+Duct Tape.";
+        assert_eq!(vec!["Pick three"], search(query, contents));
     }
 }
-
